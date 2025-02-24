197ef0bb07d4d42b63d3785ac2f3e35495fc5f2c
LAKSHAYNISCHAY <araavsinghj@gmail.com>
2025-02-24 21:00:03 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,23 @@
+const questions = [
+    {
+        question: "1. Who recently took over as the Secretary-General of NATO?",
+        options: [
+            "(a) Jens Stoltenberg",
+            "(b) Mark Rutte",
+            "(c) Mahli Sahars",
+            "(d) None of these"
+        ],
+        correctAnswer: "b"
+    },
+    {
+        question: "2. How many temporary members are there in the United Nations Security Council?",
+        options: [
+            "(a) 5",
+            "(b) 10",
+            "(c) 15",
+            "(d) 20"
+        ],
+        correctAnswer: "b"
+    }
+    // Add more questions as needed
+];
